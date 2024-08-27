3436f1c42caf89f28acc1160d4d98bfd0d373831
Abel O'Rian <abel.orian@gmail.com>
2024-08-27 10:22:55 -0400

    Update client.rb
    Fix error message

--- lib/lemon_squeezy/client.rb
+++ lib/lemon_squeezy/client.rb
@@ -14,6 +14,7 @@ module LemonSqueezy
           }
 
           conn.request :json
+
           conn.response :json
         end
       end
@@ -37,25 +38,25 @@ module LemonSqueezy
       def handle_response(response)
         case response.status
         when 400
-          raise Error.new("Error 400: Your request was malformed.", status: 400, error_details: response.body["errors"])
+          raise Error.new("Your request was malformed.", status: 400, error_details: response.body["errors"])
         when 401
-          raise Error.new("Error 401: You did not supply valid authentication credentials.", status: 401, error_details: response.body["errors"])
+          raise Error.new("You did not supply valid authentication credentials.", status: 401, error_details: response.body["errors"])
         when 403
-          raise Error.new("Error 403: You are not allowed to perform that action.", status: 403, error_details: response.body["errors"])
+          raise Error.new("You are not allowed to perform that action.", status: 403, error_details: response.body["errors"])
         when 404
-          raise Error.new("Error 404: No results were found for your request.", status: 404, error_details: response.body["errors"])
+          raise Error.new("No results were found for your request.", status: 404, error_details: response.body["errors"])
         when 409
-          raise Error.new("Error 409: Your request was a conflict.", status: 409, error_details: response.body["errors"])
+          raise Error.new("Your request was a conflict.", status: 409, error_details: response.body["errors"])
         when 429
-          raise Error.new("Error 429: Your request exceeded the API rate limit.", status: 429, error_details: response.body["errors"])
+          raise Error.new("Your request exceeded the API rate limit.", status: 429, error_details: response.body["errors"])
         when 422
-          raise Error.new("Error 422: Unprocessable Entity.", status: 422, error_details: response.body["errors"])
+          raise Error.new("Unprocessable Entity.", status: 422, error_details: response.body["errors"])
         when 500
-          raise Error.new("Error 500: We were unable to perform the request due to server-side problems.", status: 500, error_details: response.body["errors"])
+          raise Error.new("We were unable to perform the request due to server-side problems.", status: 500, error_details: response.body["errors"])
         when 503
-          raise Error.new("Error 503: You have been rate limited for sending more than 20 requests per second.", status: 503, error_details: response.body["errors"])
+          raise Error.new("You have been rate limited for sending more than 20 requests per second.", status: 503, error_details: response.body["errors"])
         when 501
-          raise Error.new("Error 501: This resource has not been implemented.", status: 501, error_details: response.body["errors"])
+          raise Error.new("This resource has not been implemented.", status: 501, error_details: response.body["errors"])
         when 204
           return true
         end
