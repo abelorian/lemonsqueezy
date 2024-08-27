5fdeb24fac40f0e9e1fba1488fc0a04f0839ca07
Abel O'Rian <abel.orian@gmail.com>
2024-08-27 10:12:59 -0400

    Update error.rb

--- lib/lemon_squeezy/error.rb
+++ lib/lemon_squeezy/error.rb
@@ -1,4 +1,11 @@
 module LemonSqueezy
   class Error < StandardError
+    attr_reader :status, :error_details
+
+    def initialize(message, status: nil, error_details: nil)
+      @status = status
+      @error_details = error_details
+      super(message)
+    end
   end
 end
